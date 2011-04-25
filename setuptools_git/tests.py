1ef99672ce88aa36dbbd3c72d0237d666fc68d31
Wichert Akkerman <wichert@wiggy.net>
2011-04-25 13:18:32 +0200

    Add more tests

--- setuptools_git/tests.py
+++ setuptools_git/tests.py
@@ -16,13 +16,14 @@ except ImportError:
         return retcode
 
 
-class Base(unittest.TestCase):
+class GitTestCase(unittest.TestCase):
     def setUp(self):
         import os
         import tempfile
-        self.directory=tempfile.mkdtemp()
-        self.old_cwd=os.getcwd()
+        self.directory = tempfile.mkdtemp()
+        self.old_cwd = os.getcwd()
         os.chdir(self.directory)
+        check_call(['git', 'init', '--quiet', self.directory])
 
     def tearDown(self):
         import os
@@ -32,28 +33,27 @@ class Base(unittest.TestCase):
 
     def create_file(self, *path):
         import os.path
-        fd=open(os.path.join(*path), 'wt')
+        fd = open(os.path.join(*path), 'wt')
         print >>fd, 'dummy'
         fd.close()
 
     def create_git_file(self, *path):
         import os.path
-        filename=os.path.join(*path)
-        fd=open(filename, 'wt')
+        filename = os.path.join(*path)
+        fd = open(filename, 'wt')
         print >>fd, 'dummy'
         fd.close()
         check_call(['git', 'add', filename])
-        check_call(['git', 'commit', '-m', 'add new file'])
+        check_call(['git', 'commit', '--quiet', '-m', 'add new file'])
 
 
-class list_git_files_tests(Base):
+class list_git_files_tests(GitTestCase):
     def list_git_files(self, *a, **kw):
         from setuptools_git import list_git_files
         return list_git_files(*a, **kw)
 
     def test_at_repo_root(self):
         import os.path
-        check_call(['git', 'init', self.directory])
         self.create_git_file('root.txt')
         self.assertEqual(
                 self.list_git_files(self.directory),
@@ -62,7 +62,6 @@ class list_git_files_tests(Base):
     def test_at_repo_subdir(self):
         import os
         import os.path
-        check_call(['git', 'init', self.directory])
         self.create_git_file('root.txt')
         os.mkdir(os.path.join(self.directory, 'subdir'))
         self.create_git_file('subdir', 'entry.txt')
@@ -71,3 +70,44 @@ class list_git_files_tests(Base):
                 set([os.path.realpath('root.txt'),
                      os.path.realpath(os.path.join('subdir', 'entry.txt'))]))
 
+
+class gitlsfiles_tests(GitTestCase):
+    def gitlsfiles(self, *a, **kw):
+        from setuptools_git import gitlsfiles
+        return gitlsfiles(*a, **kw)
+
+    def test_empty_dirname(self):
+        self.create_git_file('root.txt')
+        self.assertEqual(
+                set(self.gitlsfiles('')),
+                set(['root.txt']))
+
+    def test_specify_full_path(self):
+        import os
+        self.create_git_file('root.txt')
+        os.chdir(self.old_cwd)
+        self.assertEqual(
+                set(self.gitlsfiles(self.directory)),
+                set(['root.txt']))
+
+    def test_package_root_in_repo_subdir(self):
+        import os
+        os.mkdir(os.path.join(self.directory, 'package'))
+        self.create_git_file('package', 'root.txt')
+        self.assertEqual(
+                set(self.gitlsfiles(os.path.join(self.directory, 'package'))),
+                set(['root.txt']))
+
+    def test_directory_symlink(self):
+        import os
+        os.mkdir(os.path.join(self.directory, 'subdir'))
+        self.create_git_file('subdir', 'entry.txt')
+        os.mkdir(os.path.join(self.directory, 'package'))
+        self.create_git_file('package', 'root.txt')
+        os.symlink(
+                os.path.join(self.directory, 'subdir'),
+                os.path.join(self.directory, 'package', 'data'))
+        self.assertEqual(
+                set(self.gitlsfiles(os.path.join(self.directory, 'package'))),
+                set(['root.txt',
+                    os.path.join('data', 'entry.txt')]))
