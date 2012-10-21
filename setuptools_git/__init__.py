fc3a495c50c8857fd9121f374e7a063ffff46e0d
Stefan H. Holek <stefan@epy.co.at>
2012-10-21 20:32:25 +0200

    Use fsencode to make path comparison work under Python 3. Also, don't complain when Git is not installed.

--- setuptools_git/__init__.py
+++ setuptools_git/__init__.py
@@ -5,12 +5,13 @@ A hook into setuptools for Git.
 """
 
 import os
-import os.path
+import sys
+from os.path import realpath, join
 from subprocess import CalledProcessError
 from subprocess import PIPE
-from distutils.log import warn
+from distutils import log
 from setuptools_git.compat import check_output
-from setuptools_git.compat import b
+from setuptools_git.compat import b, fsencode
 
 
 def list_git_files(cwd):
@@ -30,35 +31,41 @@ def list_git_files(cwd):
     #
     # This is necessary for the matching done in the "if realname in
     # git_files" code in gitlsfiles to work properly.
-    git_top = check_output(['git', 'rev-parse', '--show-toplevel'],
-            stderr=PIPE, cwd=cwd).strip()
-    ls = check_output(['git', 'ls-files', '-z'], cwd=git_top, stderr=PIPE)
-    filenames = filter(None, ls.split(b('\x00'))) # filter None for trailing \x00
-    git_files = set([os.path.join(git_top, fn) for fn in filenames])
-    return git_files
+    git_top = check_output(
+        ['git', 'rev-parse', '--show-toplevel'], cwd=cwd, stderr=PIPE).strip()
+    try:
+        filenames = check_output(
+            ['git', 'ls-files', '-z'], cwd=git_top, stderr=PIPE)
+    except (CalledProcessError, OSError):
+        log.warn("%s: Error running 'git ls-files'", __name__)
+        raise
+    filenames = filter(None, filenames.split(b('\x00')))
+    filenames = [join(git_top, fn) for fn in filenames]
+    return set(filenames)
 
 
 def gitlsfiles(dirname=''):
+    if dirname:
+        cwd = dirname
+    else:
+        cwd = None
+        dirname = '.'
+
     try:
-        if dirname:
-            cwd = dirname
-        else:
-            cwd = None
-            dirname = '.'
         git_files = list_git_files(cwd)
     except (CalledProcessError, OSError):
         # Something went terribly wrong but the setuptools doc says we
         # must be strong in the face of danger.  We shall not run away
         # in panic.
-        warn('Error running git')
         raise StopIteration
 
     # Include symlinked files and directories by their symlinked path
-    dirname = os.path.realpath(dirname)
+    dirname = realpath(dirname)
     prefix_length = len(dirname) + 1
     for (root, dirs, files) in os.walk(dirname, followlinks=True):
         for file in files:
-            filename = os.path.join(root, file)
-            realname = os.path.realpath(filename)
+            filename = join(root, file)
+            realname = fsencode(realpath(filename))
             if realname in git_files:
                 yield filename[prefix_length:]
+
