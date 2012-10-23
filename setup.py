3016a94fad8854837640f620d494e3e1c539eec2
Stefan H. Holek <stefan@epy.co.at>
2012-10-23 10:36:23 +0200

    Update classifiers.

--- setup.py
+++ setup.py
@@ -30,13 +30,16 @@ setup(
         "Topic :: Software Development :: Version Control",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "License :: Public Domain",
+        "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.4",
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
         ],
     entry_points="""
         [setuptools.file_finders]
