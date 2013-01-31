36b667b33e88899d0623e0fadaff3afc1f905e4a
Visa Kopu <visa@visakopu.net>
2013-01-31 09:26:22 +0200

    Fixed typos in setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ version = '1.0b2dev'
 setup(
     name="setuptools-git",
     version=version,
-    mainainer='Wichert Akkerman',
-    mainainer_email='wichert@wiggy.net',
+    maintainer='Wichert Akkerman',
+    maintainer_email='wichert@wiggy.net',
     author="Yannick Gingras",
     author_email="ygingras@ygingras.net",
     url="https://github.com/wichert/setuptools-git",
