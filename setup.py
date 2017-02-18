948ef4d639e2c457f6dc20a622c303bd81d6588c
Marc Abramowitz <abramowi@adobe.com>
2017-02-17 16:36:19 -0800

    setup.py: Update classifiers
    - Remove 2.4, 2.5, 2.6
    - Add 3.4, 3.5, 3.6

--- setup.py
+++ setup.py
@@ -23,14 +23,14 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.4",
-        "Programming Language :: Python :: 2.5",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.1",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         ],
     entry_points="""
         [setuptools.file_finders]
