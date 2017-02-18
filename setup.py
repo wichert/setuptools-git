7d185495151185edc2b2c00d80b143ffa654a624
Marc Abramowitz <abramowi@adobe.com>
2017-02-17 16:06:34 -0800

    Set myself as maintainer

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ version = '1.2dev'
 setup(
     name="setuptools-git",
     version=version,
-    maintainer='Wichert Akkerman',
-    maintainer_email='wichert@wiggy.net',
+    maintainer='Marc Abramowitz',
+    maintainer_email='msabramo@gmail.com',
     author="Yannick Gingras",
     author_email="ygingras@ygingras.net",
     url="https://github.com/wichert/setuptools-git",
